b309078d6a37074b2b05e6a78b2d30ea34f40dd3
kajsawiola <kajsam@hotmail.com>
2023-02-22 13:28:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dag_13._skapa_gitrepo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
